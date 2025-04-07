5572b4b0e7b57a67cbeb42d5df85a67aeb187e84
Yash Mehta <yashmehta0395@gmail.com>
2025-04-07 14:23:50 +0530

    created custom bottom navigation bar and created different routes for our app

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="items-center justify-center flex-1">
+      <Text className="text-5xl font-bold text-blue-500">Welcome</Text>
+    </View>
+  );
+}
